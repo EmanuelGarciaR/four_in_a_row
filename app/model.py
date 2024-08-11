89110ea402aaa0b98d57cf2484fb7332426c4b40
Ema <emanuelgarciarios@gmail.com>
2024-08-11 12:23:23 -0500

    Create class Board

--- app/model.py
+++ app/model.py
@@ -1,6 +1,19 @@
 import numpy as np
+
+
 class Dimension:
     def __init__(self, dimension: int):
         if dimension not in [4, 5, 6]:  # Verificar dimensión en el rango permitido
             raise ValueError("Dimensión no permitida. Debe ser 4, 5 o 6.")
         self.dimension = dimension
+
+
+class Board:
+
+    def __init__(self, dimensions: Dimension):
+        self.dimensions = dimensions.dimension
+        self.matriz = np.full((self.dimensions, self.dimensions), '-')
+
+dimension = Dimension(4)
+board= Board(dimension)
+print(board.matriz)
